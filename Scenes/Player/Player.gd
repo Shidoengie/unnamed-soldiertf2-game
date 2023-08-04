9ecede0eb92b777077034c3de8d1e99a758d6014
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2023-08-04 10:40:33 +0100

    idfk

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -11,9 +11,15 @@ extends Node2D
 @onready var camera_offset = camera.global_transform.origin
 @onready var Base_CameraPos = camera.global_transform.origin
 var falling = false
+enum CameraSettings {
+	FOLLOW,
+	ONGROUND_FOLLOW,
+}
+@export var cur_CameraSettings = CameraSettings.ONGROUND_FOLLOW
 @onready var CameraValues = {
 	CROUCH = Vector2(0,100) + Base_CameraPos,
-	LOOK_DOWN = Vector2(0,100) + Base_CameraPos, LOOK_UP = Vector2(0,-70) + Base_CameraPos,
+	LOOK_DOWN = Vector2(0,100) + Base_CameraPos,
+	LOOK_UP = Vector2(0,-70) + Base_CameraPos,
 }
 func _process(delta):
 	if Input.is_action_pressed("look_down"):
@@ -24,17 +30,19 @@ func _process(delta):
 		camera_offset = CameraValues.CROUCH
 	elif player_visible.is_on_screen() and GroundStates.current != GroundStates.FALL:
 		camera_offset = Base_CameraPos
-	
 	camera.global_transform.origin.x = player_body.position.x+camera_offset.x
+	match cur_CameraSettings:
+		CameraSettings.FOLLOW:
+			camera.global_transform.origin.y = player_body.position.y+camera_offset.y
+		CameraSettings.ONGROUND_FOLLOW:
+			_onground_follow()
+func _onground_follow():
 	if player_body.is_on_floor():
 		falling = false
 	if falling:
 		camera.global_transform.origin.y = player_body.global_transform.origin.y+150
 	elif GroundStates.current != GroundStates.JUMP or (Input.is_action_just_released("look_down") or Input.is_action_just_released("look_up")):
 		camera.global_transform.origin.y = player_body.position.y+camera_offset.y
-	
-	
-
 func _on_player_visible_screen_exited():
 	if GroundStates.current == GroundStates.FALL:
 		falling = true
